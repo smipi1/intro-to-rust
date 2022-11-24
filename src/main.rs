65aa4f466c4337da68bd9224587664d70c6ca0de
Pieter Smith <pieter@bsentient.ai>
2022-11-24 12:06:43 +0100

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    println!("Guess the number");
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+        
+        let mut guess = String::new();
+        
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(err) => {
+                println!("Error: {err}");
+                continue;
+            }
+        };
+        
+        println!("You guessed: {guess}");
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You guessed right!");
+                break;
+            },
+        }
+    }
 }
