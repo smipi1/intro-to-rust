30d204db0397146ac3d85851547ac4d553cdf3ba
Pieter Smith <pieter@boesman.nl>
2022-11-29 11:22:02 +0100

    add liftoff example

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 fn main() {
-    let number = 3;
-
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
+    for number in (1..4).rev() {
+        println!("{number}!");
     }
+    println!("LIFTOFF!!!");
 }
